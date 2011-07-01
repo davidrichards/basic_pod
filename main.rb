d4e04985ef46fc2f96e93c894fec02a536790d1f
David Richards <davidlamontrichards@gmail.com>
2011-06-30 19:47:29 -0600

    Updating the pod for the new/tested snap process

--- main.rb
+++ main.rb
@@ -1,14 +1,14 @@
-class UserPod < PodAgent::DSL
-
-  name "A Basic Pod"
-
-  description "A very good test for whether the Ubalo environment is working.  This shouldn't work in the wild, btw."
+class UserPod
+  include UbaloPod::Behavior
   
-  def initialize(*args)
-  end
+  name "Super Pod"
+  description "A very useful pod"
+  
+  input :input_n, "The amount of super we started with", 100, numeric: true
+  output :output_n, "The amount of super we produced"
   
   def snap
-    puts Math.cos(1.3 * Math::PI)
+    n = input_params[:input_n] * 1_000_000
+    write_output(output_n: n)
   end
-  
 end
